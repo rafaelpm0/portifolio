39ed22a6934943b0a1e707f74e91c6ef8aa7defa
Rafael Pinho Medeiros <rafaelpmedeiros00@gmail.com>
2025-01-14 21:03:00 -0300

    iniciando a populacao de demais informacoes na top bar e foto inicial

--- src/components/Home/Carrossel/Card.tsx
+++ src/components/Home/Carrossel/Card.tsx
@@ -22,12 +22,12 @@ function Card({hobby} : CardProps) {
   const { title, image } = hobby;
   
   return (
-    <UICard className="h-[220px]">
+    <UICard>
       <CardHeader>
         <CardTitle>{title}</CardTitle>
       </CardHeader>
-      <CardContent className="flex items-center">
-        <img src={image} alt={title} className="h-[100%] w-[100%]"/>
+      <CardContent className="overflow-hidden">
+        <img src={image} alt={title} className="h-full w-full max-h-[64px] sm:max-h-[110px] object-cover object-center"/>
       </CardContent>
     </UICard>
   );
---
+++ src/components/header/ButtonHiddenContent/Contact.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contato</h2>
+      <ul>
+        <li>Telefone:</li>
+        <li>email: </li>
+        <li>Linkedin</li>
+        <li>Instagran</li>
+        <li>GitHub</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Contact
